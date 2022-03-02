400c1d24e84640635abec71416af2ba069850caf
fyofio <muhammadisrofiordi@gmail.com>
2022-03-03 00:38:14 +0700

    improvement in details parser

--- parsers/details.rb
+++ parsers/details.rb
@@ -3,9 +3,11 @@ nokogiri = Nokogiri.HTML(content)
 
 # get the seller username
 seller = nokogiri.at_css('.seller-persona a')&.text
+seller = nokogiri.at_css('.ux-seller-section__item--seller > a:nth-child(1) > span:nth-child(1)')&.text if seller.nil?
 
 # get the seller's feedback
 feedback = nokogiri.at_css('.seller-persona span[3]')&.text
+feedback = nokogiri.at_css('div.ux-seller-section:nth-child(2) > div:nth-child(1) > div:nth-child(2) > span:nth-child(1)')&.text if feedback.nil?
 
 # save it into outputs
 outputs << {
